22216296320b40ae6d79d18687347f23d6e9c9a5
themihabyte <mihabayt@gmail.com>
2020-03-25 22:05:12 +0200

    Displays all fragments

--- app/src/main/java/m/novikov/io/github/themihabyte/kievtourguide/MainActivity.java
+++ app/src/main/java/m/novikov/io/github/themihabyte/kievtourguide/MainActivity.java
@@ -4,11 +4,24 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.main_activity);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        FragmentAdapter fragmentPagerAdapter = new FragmentAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+                this);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
